6564c7abf8ed32cf4f272148efc7d5e9c6e29f1f
William Stark <william@ebi.ac.uk>
2022-05-11 15:54:56 +0100

    use a NOTICE file for licensing information

--- ensembl_anno.py
+++ ensembl_anno.py
@@ -1,10 +1,11 @@
-# Copyright [2019-2021] EMBL-European Bioinformatics Institute
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
