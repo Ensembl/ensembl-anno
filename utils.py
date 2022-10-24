a8fc6e1ddea7c7f950d2f2fb1d7c44223c6bbe0d
ens-ftricomi <ftricomi@ebi.ac.uk>
2022-10-24 11:29:06 +0100

    moved load result to core db option

--- utils.py
+++ utils.py
@@ -86,7 +86,6 @@ def check_gtf_content(gtf_file: Union[pathlib.Path, str], content_obj):
 def create_dir(main_output_dir: Union[pathlib.Path, str], dir_name: str = None):
     """
     Create directory or subdirectory and log operations.
-
     Args:
         main_output_dir: main output directory path
         dir_name: optional subdirectory to be created
@@ -172,11 +171,9 @@ def get_seq_region_lengths(genome_file: Union[pathlib.Path, str], min_seq_length
 def prlimit_command(command_list: list, virtual_memory_limit: int):
     """
     Uses the `prlimit` program to set a memory limit for a command list to be run with subprocess.
-
     prlimit - get and set process resource limits
     -v, --as[=limits]
         Address space limit.
-
     Args:
         command_list: original subprocess command list
         virtual_memory_limit: virtual memory limit in bytes
@@ -186,24 +183,21 @@ def prlimit_command(command_list: list, virtual_memory_limit: int):
     return ["prlimit", f"-v{virtual_memory_limit}"] + command_list
 
 def load_results_to_ensembl_db(
-    main_script_dir,
+    main_script_dir: pathlib.Path,
     load_to_ensembl_db,
-    genome_file,
-    main_output_dir,
+    genome_file: Union[pathlib.Path, str],
+    main_output_dir: pathlib.Path,
     db_details,
-    num_threads,
+    num_threads: int,
 ):
-
-    db_loading_script = os.path.join(
-        main_script_dir, "support_scripts_perl", "load_gtf_ensembl.pl"
-    )
+    db_loading_script = main_script_dir / "support_scripts_perl" / "load_gtf_ensembl.pl"
     db_loading_dir = create_dir(main_output_dir, "db_loading")
 
     # Should collapse this into a function
-    annotation_results_gtf_file = os.path.join(
-        main_output_dir, "annotation_output", "annotation.gtf"
+    annotation_results_gtf_file = (
+        main_output_dir / "annotation_output" / "annotation.gtf"
     )
-    if os.path.exists(annotation_results_gtf_file):
+    if annotation_results_gtf_file.exists():
         logger.info("Loading main geneset to db")
         batch_size = 200
         load_type = "gene"
@@ -224,14 +218,12 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find the main gene annotation file, so not loading. Path checked:\n"
-            + annotation_results_gtf_file
+            "Main gene annotation file not found, can't load:\n%s"
+            % annotation_results_gtf_file
         )
 
-    rfam_results_gtf_file = os.path.join(
-        main_output_dir, "rfam_output", "annotation.gtf"
-    )
-    if os.path.exists(rfam_results_gtf_file):
+    rfam_results_gtf_file = main_output_dir / "rfam_output" / "annotation.gtf"
+    if rfam_results_gtf_file.exists():
         logger.info("Loading Rfam-based sncRNA genes to db")
         batch_size = 500
         load_type = "gene"
@@ -252,14 +244,11 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find an Rfam annotation file, so not loading. Path checked:\n"
-            + rfam_results_gtf_file
+            "Rfam annotation file not found, can't load:\n%s" % rfam_results_gtf_file
         )
 
-    trnascan_results_gtf_file = os.path.join(
-        main_output_dir, "trnascan_output", "annotation.gtf"
-    )
-    if os.path.exists(trnascan_results_gtf_file):
+    trnascan_results_gtf_file = main_output_dir / "trnascan_output" / "annotation.gtf"
+    if trnascan_results_gtf_file.exists():
         logger.info("Loading tRNAScan-SE tRNA genes to db")
         batch_size = 500
         load_type = "gene"
@@ -280,14 +269,12 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find an tRNAScan-SE annotation file, so not loading. Path checked:\n"
-            + trnascan_results_gtf_file
+            "tRNAScan-SE annotation file not found, can't load:\n%s"
+            % trnascan_results_gtf_file
         )
 
-    dust_results_gtf_file = os.path.join(
-        main_output_dir, "dust_output", "annotation.gtf"
-    )
-    if os.path.exists(dust_results_gtf_file):
+    dust_results_gtf_file = main_output_dir / "dust_output" / "annotation.gtf"
+    if dust_results_gtf_file.exists():
         logger.info("Loading Dust repeats to db")
         batch_size = 500
         load_type = "single_line_feature"
@@ -308,12 +295,11 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find a Dust annotation file, so not loading. Path checked:\n"
-            + dust_results_gtf_file
+            "Dust annotation file not found, can't load:\n%s" % dust_results_gtf_file
         )
 
-    red_results_gtf_file = os.path.join(main_output_dir, "red_output", "annotation.gtf")
-    if os.path.exists(red_results_gtf_file):
+    red_results_gtf_file = main_output_dir / "red_output" / "annotation.gtf"
+    if red_results_gtf_file.exists():
         logger.info("Loading Red repeats to db")
         batch_size = 500
         load_type = "single_line_feature"
@@ -334,12 +320,11 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find a Red annotation file, so not loading. Path checked:\n"
-            + red_results_gtf_file
+            "Red annotation file not found, can't load:\n%s" % red_results_gtf_file
         )
 
-    trf_results_gtf_file = os.path.join(main_output_dir, "trf_output", "annotation.gtf")
-    if os.path.exists(trf_results_gtf_file):
+    trf_results_gtf_file = main_output_dir / "trf_output" / "annotation.gtf"
+    if trf_results_gtf_file.exists():
         logger.info("Loading TRF repeats to db")
         batch_size = 500
         load_type = "single_line_feature"
@@ -360,12 +345,11 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find a TRF annotation file, so not loading. Path checked:\n"
-            + trf_results_gtf_file
+            "TRF annotation file not found, can't load:\n%s" % trf_results_gtf_file
         )
 
-    cpg_results_gtf_file = os.path.join(main_output_dir, "cpg_output", "annotation.gtf")
-    if os.path.exists(cpg_results_gtf_file):
+    cpg_results_gtf_file = main_output_dir / "cpg_output" / "annotation.gtf"
+    if cpg_results_gtf_file.exists():
         logger.info("Loading CpG islands to db")
         batch_size = 500
         load_type = "single_line_feature"
@@ -386,14 +370,11 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find a CpG annotation file, so not loading. Path checked:\n"
-            + cpg_results_gtf_file
+            "CpG annotation file not found, not loading:\n%s" % cpg_results_gtf_file
         )
 
-    eponine_results_gtf_file = os.path.join(
-        main_output_dir, "eponine_output", "annotation.gtf"
-    )
-    if os.path.exists(eponine_results_gtf_file):
+    eponine_results_gtf_file = main_output_dir / "eponine_output" / "annotation.gtf"
+    if eponine_results_gtf_file.exists():
         logger.info("Loading Eponine repeats to db")
         batch_size = 500
         load_type = "single_line_feature"
@@ -414,8 +395,8 @@ def load_results_to_ensembl_db(
         )
     else:
         logger.error(
-            "Did not find an Eponine annotation file, so not loading. Path checked:\n"
-            + eponine_results_gtf_file
+            "Eponine annotation file not found, can't load:\n%s"
+            % eponine_results_gtf_file
         )
 
     logger.info("Finished loading records to db")
@@ -424,16 +405,15 @@ def load_results_to_ensembl_db(
 def generic_load_records_to_ensembl_db(
     load_to_ensembl_db,
     db_loading_script,
-    genome_file,
+    genome_file: Union[pathlib.Path, str],
     db_details,
     db_loading_dir,
     load_type,
     analysis_name,
     gtf_records,
-    num_threads,
+    num_threads: int,
 ):
-
-    pool = multiprocessing.Pool(int(num_threads))
+    pool = multiprocessing.Pool(num_threads)
     for record_batch in gtf_records:
         pool.apply_async(
             multiprocess_load_records_to_ensembl_db,
@@ -456,22 +436,21 @@ def generic_load_records_to_ensembl_db(
 def multiprocess_load_records_to_ensembl_db(
     load_to_ensembl_db,
     db_loading_script,
-    genome_file,
+    genome_file: Union[pathlib.Path, str],
     db_details,
     output_dir,
     load_type,
     analysis_name,
     record_batch,
 ):
-
     with tempfile.NamedTemporaryFile(
         mode="w+t", delete=False, dir=output_dir
     ) as gtf_temp_out:
         for line in record_batch:
-            gtf_temp_out.writelines(line)
+            gtf_temp_out.write(line)
             gtf_temp_file_path = gtf_temp_out.name
 
-    (db_name, db_host, db_port, db_user, db_pass) = db_details.split(",")
+    db_name, db_host, db_port, db_user, db_pass = db_details.split(",")
 
     loading_cmd = [
         "perl",
@@ -509,144 +488,71 @@ def multiprocess_load_records_to_ensembl_db(
         if load_to_ensembl_db == "single_transcript_genes":
             loading_cmd.append("-make_single_transcript_genes")
 
-    logger.info(" ".join(loading_cmd))
+    logger.info("loading_cmd: %s" % " ".join(loading_cmd))
     subprocess.run(loading_cmd)
     gtf_temp_out.close()
-    os.remove(gtf_temp_file_path)  # doesn't seem to be working
-    logger.info("Finished: " + gtf_temp_file_path)
+    os.remove(gtf_temp_file_path)  # NOTE: doesn't seem to be working
+    logger.info("Finished: %s" % gtf_temp_file_path)
     gc.collect()
 
 
 def batch_gtf_records(input_gtf_file, batch_size, output_dir, record_type):
-
     records = []
-    gtf_in = open(input_gtf_file)
-    if record_type == "gene":
+    with open(input_gtf_file) as gtf_in:
+        if record_type == "gene":
+            # NOTE that the neverending variations on GTF reading/writing/merging is becoming very messy
+            # need to create a set of utility methods outside of this script
+            # This one assumes the file has unique ids for the parent features. It then batches them into
+            # sets of records based on the batch size passed in
+            record_counter = 0
+            current_record_batch = []
+            current_gene_id = ""
+            for line in gtf_in:
+                if re.search(r"^#", line):
+                    continue
 
-        # NOTE that the neverending variations on GTF reading/writing/merging is becoming very messy
-        # need to create a set of utility methods outside of this script
-        # This one assumes the file has unique ids for the parent features. It then batches them into
-        # sets of records based on the batch size passed in
-        record_counter = 0
-        current_record_batch = []
-        current_gene_id = ""
-        line = gtf_in.readline()
-        while line:
-            if re.search(r"^#", line):
-                line = gtf_in.readline()
-                continue
+                eles = line.split("\t")
+                if not len(eles) == 9:
+                    continue
 
-            eles = line.split("\t")
-            if not len(eles) == 9:
-                line = gtf_in.readline()
-                continue
+                match = re.search(r'gene_id "([^"]+)"', line)
+                gene_id = match.group(1)
 
-            match = re.search(r'gene_id "([^"]+)"', line)
-            gene_id = match.group(1)
+                if not current_gene_id:
+                    record_counter += 1
+                    current_gene_id = gene_id
 
-            if not current_gene_id:
+                if not gene_id == current_gene_id:
+                    record_counter += 1
+                    if record_counter % batch_size == 0:
+                        records.append(current_record_batch)
+                        current_record_batch = []
+                    current_gene_id = gene_id
+
+                current_record_batch.append(line)
+
+            records.append(current_record_batch)
+
+        elif record_type == "single_line_feature":
+            record_counter = 0
+            current_record_batch = []
+            current_gene_id = ""
+            for line in gtf_in:
+                if re.search(r"^#", line):
+                    continue
+
+                eles = line.split("\t")
+                if not len(eles) == 9:
+                    continue
+
                 record_counter += 1
-                current_gene_id = gene_id
 
-            if not gene_id == current_gene_id:
-                record_counter += 1
                 if record_counter % batch_size == 0:
                     records.append(current_record_batch)
                     current_record_batch = []
-                current_gene_id = gene_id
 
-            current_record_batch.append(line)
-            line = gtf_in.readline()
+                current_record_batch.append(line)
 
-        records.append(current_record_batch)
+            records.append(current_record_batch)
 
-    elif record_type == "single_line_feature":
-        record_counter = 0
-        current_record_batch = []
-        current_gene_id = ""
-        line = gtf_in.readline()
-        while line:
-            if re.search(r"^#", line):
-                line = gtf_in.readline()
-                continue
-
-            eles = line.split("\t")
-            if not len(eles) == 9:
-                line = gtf_in.readline()
-                continue
-
-            record_counter += 1
-
-            if record_counter % batch_size == 0:
-                records.append(current_record_batch)
-                current_record_batch = []
-
-            current_record_batch.append(line)
-            line = gtf_in.readline()
-
-        records.append(current_record_batch)
-
-    gtf_in.close()
-
-    return records
-
-def run_find_orfs(genome_file, main_output_dir):
-
-    min_orf_length = 600
-
-    orf_output_dir = create_dir(main_output_dir, "orf_output")
-    seq_region_lengths = get_seq_region_lengths(genome_file, 5000)
-    for region_name in seq_region_lengths:
-        region_length = seq_region_lengths[region_name]
-        seq = get_sequence(
-            region_name, 1, region_length, 1, genome_file, orf_output_dir
-        )
-        for phase in range(0, 6):
-            find_orf_phased_region(
-                region_name, seq, phase, min_orf_length, orf_output_dir
-            )
-
-
-def find_orf_phased_region(region_name, seq, phase, min_orf_length, orf_output_dir):
-
-    current_index = phase
-    orf_counter = 1
-    if phase > 2:
-        seq = reverse_complement(seq)
-        current_index = current_index % 3
-
-    orf_file_path = os.path.join(
-        orf_output_dir, (region_name + ".phase" + str(phase) + ".orf.fa")
-    )
-    orf_out = open(orf_file_path, "w+")
-
-    while current_index < len(seq):
-        codon = seq[current_index : current_index + 3]
-        if codon == "ATG":
-            orf_seq = codon
-            for j in range(current_index + 3, len(seq), 3):
-                next_codon = seq[j : j + 3]
-                if next_codon == "TAA" or next_codon == "TAG" or next_codon == "TGA":
-                    orf_seq += next_codon
-                    if len(orf_seq) >= min_orf_length:
-                        orf_out.write(
-                            ">"
-                            + region_name
-                            + "_phase"
-                            + str(phase)
-                            + "_orf"
-                            + str(orf_counter)
-                            + "\n"
-                        )
-                        orf_out.write(orf_seq + "\n")
-                        orf_counter += 1
-                        orf_seq = ""
-                        break
-
-                # If there's another met in phase, then put i to the start of the codon after j so that only the longest ORF is found
-                if next_codon == "ATG":
-                    current_index = j + 3
-                orf_seq += next_codon
-        current_index += 3
-    orf_out.close()
-
+    return records    
