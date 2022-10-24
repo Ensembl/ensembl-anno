7e1bc05a5ff4af08303bc9a034bc00a99b3dce24
ens-ftricomi <ftricomi@ebi.ac.uk>
2022-10-24 14:55:28 +0100

    moved dust and trf to repeatmasking

--- repeatmasking_utils.py
+++ repeatmasking_utils.py
@@ -183,3 +183,247 @@ def create_repeatmasker_gtf(
                 repeat_count += 1
 
 
+def run_dust_regions(
+    genome_file: Union[pathlib.Path, str], dust_path, main_output_dir, num_threads: int
+):
+    if not dust_path:
+        dust_path = "dustmasker"
+
+    check_exe(dust_path)
+    dust_output_dir = create_dir(main_output_dir, "dust_output")
+
+    output_file = dust_output_dir / "annotation.gtf"
+    logger.info("output_file: %s" % output_file)
+    if output_file.is_file():
+        transcript_count = check_gtf_content(output_file, "repeat")
+        if transcript_count > 0:
+            logger.info("Dust gtf file already exists, skipping analysis")
+            return 0
+
+    logger.info("Creating list of genomic slices")
+    seq_region_lengths = get_seq_region_lengths(genome_file, min_seq_length=5000)
+    slice_ids = create_slice_ids(
+        seq_region_lengths, slice_size=1_000_000, overlap=0, min_length=5000
+    )
+
+    generic_dust_cmd = [dust_path, "-in"]
+    logger.info("Running Dust processes")
+    pool = multiprocessing.Pool(num_threads)
+    tasks = []
+    for slice_id in slice_ids:
+        pool.apply_async(
+            multiprocess_dust,
+            args=(
+                generic_dust_cmd,
+                slice_id,
+                genome_file,
+                dust_output_dir,
+            ),
+        )
+
+    pool.close()
+    pool.join()
+    slice_output_to_gtf(dust_output_dir, ".dust.gtf", 1, "repeat_id", "dust")
+
+
+def multiprocess_dust(
+    generic_dust_cmd,
+    slice_id,
+    genome_file: Union[pathlib.Path, str],
+    dust_output_dir: pathlib.Path,
+):
+    region_name = slice_id[0]
+    start = slice_id[1]
+    end = slice_id[2]
+
+    logger.info(
+        "Processing slice to find low complexity regions with Dust: %s:%s:%s"
+        % (region_name, start, end)
+    )
+    seq = get_sequence(
+        seq_region=region_name,
+        start=start,
+        end=end,
+        strand=1,
+        fasta_file=genome_file,
+        output_dir=dust_output_dir,
+    )
+
+    slice_file_name = f"{region_name}.rs{start}.re{end}"
+    region_fasta_file_name = f"{slice_file_name}.fa"
+    region_fasta_file_path = dust_output_dir / region_fasta_file_name
+    with open(region_fasta_file_path, "w+") as region_fasta_out:
+        region_fasta_out.write(f">{region_name}\n{seq}\n")
+
+    region_results_file_name = f"{slice_file_name}.dust.gtf"
+    region_results_file_path = dust_output_dir / region_results_file_name
+
+    dust_output_file_path = f"{region_fasta_file_path}.dust"
+    dust_out = open(dust_output_file_path, "w+")
+    dust_cmd = generic_dust_cmd.copy()
+    dust_cmd.append(region_fasta_file_path)
+    logger.info("dust_cmd" % " ".join(dust_cmd))
+    subprocess.run(dust_cmd, stdout=dust_out)
+    dust_out.close()
+
+    create_dust_gtf(dust_output_file_path, region_results_file_path, region_name)
+    os.remove(dust_output_file_path)
+    os.remove(region_fasta_file_path)
+
+
+def create_dust_gtf(dust_output_file_path, region_results_file_path, region_name):
+    with open(dust_output_file_path, "r") as dust_in, open(
+        region_results_file_path, "w+"
+    ) as dust_out:
+        repeat_count = 1
+        for line in dust_in:
+            result_match = re.search(r"(\d+)\ - (\d+)", line)
+            if result_match:
+                start = int(result_match.group(1)) + 1
+                end = int(result_match.group(2)) + 1
+                gtf_line = f'{region_name}\tDust\trepeat\t{start}\t{end}\t.\t+\t.\trepeat_id "{repeat_count}";\n'
+                dust_out.write(gtf_line)
+                repeat_count += 1
+
+
+def run_trf_repeats(
+    genome_file: Union[pathlib.Path, str], trf_path, main_output_dir, num_threads: int
+):
+    if not trf_path:
+        trf_path = "trf"
+
+    check_exe(trf_path)
+    trf_output_dir = create_dir(main_output_dir, "trf_output")
+
+    output_file = trf_output_dir / "annotation.gtf"
+    if output_file.exists():
+        transcript_count = check_gtf_content(output_file, "repeat")
+        if transcript_count > 0:
+            logger.info("Trf gtf file already exists, skipping analysis")
+            return
+
+    logger.info("Creating list of genomic slices")
+    seq_region_lengths = get_seq_region_lengths(genome_file, min_seq_length=5000)
+    slice_ids = create_slice_ids(
+        seq_region_lengths, slice_size=1_000_000, overlap=0, min_length=5000
+    )
+
+    match_score = 2
+    mismatch_score = 5
+    delta = 7
+    pm = 80
+    pi = 10
+    minscore = 40
+    maxperiod = 500
+
+    trf_output_extension = (
+        f".{match_score}.{mismatch_score}.{delta}.{pm}.{pi}.{minscore}.{maxperiod}.dat"
+    )
+
+    generic_trf_cmd = [
+        trf_path,
+        None,
+        str(match_score),
+        str(mismatch_score),
+        str(delta),
+        str(pm),
+        str(pi),
+        str(minscore),
+        str(maxperiod),
+        "-d",
+        "-h",
+    ]
+    logger.info("Running TRF processes")
+    pool = multiprocessing.Pool(num_threads)
+    tasks = []
+    for slice_id in slice_ids:
+        pool.apply_async(
+            multiprocess_trf,
+            args=(
+                generic_trf_cmd,
+                slice_id,
+                genome_file,
+                trf_output_dir,
+                trf_output_extension,
+            ),
+        )
+
+    pool.close()
+    pool.join()
+    slice_output_to_gtf(trf_output_dir, ".trf.gtf", 1, "repeat_id", "trf")
+
+
+def multiprocess_trf(
+    generic_trf_cmd,
+    slice_id,
+    genome_file: Union[pathlib.Path, str],
+    trf_output_dir: pathlib.Path,
+    trf_output_extension,
+):
+    region_name = slice_id[0]
+    start = slice_id[1]
+    end = slice_id[2]
+
+    logger.info(
+        "Processing slice to find tandem repeats with TRF: %s:%s:%s"
+        % (region_name, start, end)
+    )
+    seq = get_sequence(
+        seq_region=region_name,
+        start=start,
+        end=end,
+        strand=1,
+        fasta_file=genome_file,
+        output_dir=trf_output_dir,
+    )
+
+    slice_file_name = f"{region_name}.rs{start}.re{end}"
+    region_fasta_file_name = f"{slice_file_name}.fa"
+    region_fasta_file_path = trf_output_dir / region_fasta_file_name
+    with open(region_fasta_file_path, "w+") as region_fasta_out:
+        region_fasta_out.write(f">{region_name}\n{seq}\n")
+
+    region_results_file_name = f"{slice_file_name}.trf.gtf"
+    region_results_file_path = trf_output_dir / region_results_file_name
+
+    # TRF writes to the current dir, so switch to the output dir for it
+    os.chdir(trf_output_dir)
+    trf_output_file_path = f"{region_fasta_file_path}{trf_output_extension}"
+    trf_cmd = generic_trf_cmd.copy()
+    trf_cmd[1] = region_fasta_file_path
+    logger.info("trf_cmd: %s" % " ".join(trf_cmd))
+    subprocess.run(trf_cmd)
+    create_trf_gtf(trf_output_file_path, region_results_file_path, region_name)
+    os.remove(trf_output_file_path)
+    os.remove(region_fasta_file_path)
+
+
+def create_trf_gtf(trf_output_file_path, region_results_file_path, region_name):
+    with open(trf_output_file_path, "r") as trf_in, open(
+        region_results_file_path, "w+"
+    ) as trf_out:
+        repeat_count = 1
+        for line in trf_in:
+            result_match = re.search(r"^\d+", line)
+            if result_match:
+                results = line.split()
+                if not len(results) == 15:
+                    continue
+                start = results[0]
+                end = results[1]
+                period = float(results[2])
+                copy_number = float(results[3])
+                percent_matches = float(results[5])
+                score = float(results[7])
+                repeat_consensus = results[13]
+                if (
+                    score < 50
+                    and percent_matches >= 80
+                    and copy_number > 2
+                    and period < 10
+                ) or (copy_number >= 2 and percent_matches >= 70 and score >= 50):
+                    gtf_line = f'{region_name}\tTRF\trepeat\t{start}\t{end}\t.\t+\t.\trepeat_id "{repeat_count}"; score "{score}"; repeat_consensus "{repeat_consensus}";\n'
+                    trf_out.write(gtf_line)
+                    repeat_count += 1
+
+
