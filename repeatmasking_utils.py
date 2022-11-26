efa2b3c4f4e99bbd0c40b1dc1e5e394bba0ced69
ens-ftricomi <ftricomi@ebi.ac.uk>
2022-11-26 00:59:26 +0000

    reformattted trf and red

--- repeatmasking_utils.py
+++ repeatmasking_utils.py
@@ -30,7 +30,7 @@ def run_repeatmasker_regions(
     repeatmasker_path: str,
     library: str,
     species: str,
-    main_output_dir: Union[pathlib.Path, str],
+    main_output_dir: str,
     num_threads: int,
 ):
     """
@@ -56,9 +56,7 @@ def run_repeatmasker_regions(
         library = "homo"
 
     utils.check_exe(repeatmasker_path)
-    repeatmasker_output_dir = Path(
-        utils.create_dir(main_output_dir, "repeatmasker_output")
-    )
+    repeatmasker_output_dir = Path(create_dir(main_output_dir, "repeatmasker_output"))
     os.chdir(repeatmasker_output_dir)
 
     output_file = repeatmasker_output_dir / "annotation.gtf"
@@ -70,7 +68,9 @@ def run_repeatmasker_regions(
 
     logger.info("Creating list of genomic slices")
     seq_region_lengths = get_seq_region_lengths(genome_file, 5000)
-    slice_ids = create_slice_ids(seq_region_lengths, 1000000, 0, 5000)
+    slice_ids = create_slice_ids(
+        seq_region_lengths, slice_size=1000000, overlap=0, min_length=5000
+    )
 
     if not library:
         if not species:
@@ -221,9 +221,9 @@ def create_repeatmasker_gtf(
 
 def run_dust_regions(
     genome_file: Union[pathlib.Path, str],
-    dust_path: Union[pathlib.Path, str],
-    main_output_dir,
-    num_threads,
+    dust_path: str,
+    main_output_dir: str,
+    num_threads: int,
 ):
     """
     Run Dust on genomic slices
@@ -243,7 +243,7 @@ def run_dust_regions(
 
     utils.check_exe(dust_path)
     dust_output_dir = Path(create_dir(main_output_dir, "dust_output"))
-
+    os.chdir(str(dust_output_dir))
     output_file = dust_output_dir / "annotation.gtf"
     logger.info(f"dust output {output_file}")
     if output_file.is_file():
@@ -254,7 +254,9 @@ def run_dust_regions(
 
     logger.info("Creating list of genomic slices")
     seq_region_lengths = get_seq_region_lengths(genome_file, 5000)
-    slice_ids = create_slice_ids(seq_region_lengths, 1000000, 0, 5000)
+    slice_ids = create_slice_ids(
+        seq_region_lengths, slice_size=1000000, overlap=0, min_length=5000
+    )
 
     generic_dust_cmd = [dust_path, "-in"]
     logger.info("Running Dust")
@@ -338,39 +340,40 @@ def create_dust_gtf(dust_output_file_path, region_results_file_path, region_name
                 repeat_count += 1
 
 
-def run_trf_repeats(genome_file, trf_path, main_output_dir, num_threads):
+def run_trf_repeats(
+    genome_file: Union[pathlib.Path, str], trf_path, main_output_dir, num_threads: int
+):
     """
-    Run Dust on genomic slices
+    Run trf on genomic slices
 
     Args:
         genome_file : pathlib.Path
-        dust_path : str
+        trf_path : str
         main_output_dir : pathlib.Path
         num_threads: int
 
     Return:
-        gtfs with the masked sequence for each genome slice
+        gtfs with the masked sequence for each genomic slice
     """
 
     if not trf_path:
         trf_path = "trf"
 
     check_exe(trf_path)
-    trf_output_dir = create_dir(main_output_dir, "trf_output")
-
-    logger.info("Skip analysis if the gtf file already exists")
-    output_file = os.path.join(trf_output_dir, "annotation.gtf")
-    if os.path.exists(output_file):
+    trf_output_dir = Path(create_dir(main_output_dir, "trf_output"))
+    os.chdir(str(trf_output_dir))
+    output_file = trf_output_dir / "annotation.gtf"
+    if output_file.exists():
         transcript_count = check_gtf_content(output_file, "repeat")
         if transcript_count > 0:
-            logger.info("Trf gtf file exists")
+            logger.info("Trf gtf file exists, skipping analysis")
             return
-    else:
-        logger.info("No gtf file, go on with the analysis")
 
     logger.info("Creating list of genomic slices")
     seq_region_lengths = get_seq_region_lengths(genome_file, 5000)
-    slice_ids = create_slice_ids(seq_region_lengths, 1000000, 0, 5000)
+    slice_ids = create_slice_ids(
+        seq_region_lengths, slice_size=1000000, overlap=0, min_length=5000
+    )
 
     match_score = 2
     mismatch_score = 5
@@ -381,21 +384,7 @@ def run_trf_repeats(genome_file, trf_path, main_output_dir, num_threads):
     maxperiod = 500
 
     trf_output_extension = (
-        "."
-        + str(match_score)
-        + "."
-        + str(mismatch_score)
-        + "."
-        + str(delta)
-        + "."
-        + str(pm)
-        + "."
-        + str(pi)
-        + "."
-        + str(minscore)
-        + "."
-        + str(maxperiod)
-        + ".dat"
+        f".{match_score}.{mismatch_score}.{delta}.{pm}.{pi}.{minscore}.{maxperiod}.dat"
     )
 
     generic_trf_cmd = [
@@ -438,118 +427,122 @@ def multiprocess_trf(
     trf_output_dir,
     trf_output_extension,
 ):
-
+    """
+    Run Dust on multiprocess on genomic slices
+    Args:
+        generic_trf_cmd:list
+        slice_id:str
+        genome_file : pathlib.Path
+        trf_output_dir : pathlib.Path
+        trf_output_extension: str
+    """
     region_name = slice_id[0]
     start = slice_id[1]
     end = slice_id[2]
 
     logger.info(
-        "Processing slice to find tandem repeats with TRF: "
-        + region_name
-        + ":"
-        + str(start)
-        + ":"
-        + str(end)
+        f"Processing slice to find tandem repeats with TRF: {region_name}:{start}:{end}"
     )
     seq = get_sequence(region_name, start, end, 1, genome_file, trf_output_dir)
 
-    slice_file_name = region_name + ".rs" + str(start) + ".re" + str(end)
-    region_fasta_file_name = slice_file_name + ".fa"
-    region_fasta_file_path = os.path.join(trf_output_dir, region_fasta_file_name)
-    region_fasta_out = open(region_fasta_file_path, "w+")
-    region_fasta_out.write(">" + region_name + "\n" + seq + "\n")
-    region_fasta_out.close()
+    slice_file_name = f"{region_name}.rs{start}.re{end}"
+    region_fasta_file_path = trf_output_dir / f"{slice_file_name}.fa"
+    with open(region_fasta_file_path, "w+") as region_fasta_out:
+        region_fasta_out.write(f">{region_name}\n{seq}\n")
 
     region_results_file_name = slice_file_name + ".trf.gtf"
-    region_results_file_path = os.path.join(trf_output_dir, region_results_file_name)
+    region_results_file_path = trf_output_dir / f"{slice_file_name}.trf.gtf"
 
     # TRF writes to the current dir, so swtich to the output dir for it
     os.chdir(trf_output_dir)
-    trf_output_file_path = region_fasta_file_path + trf_output_extension
+    trf_output_file_path = f"{region_fasta_file_path}{trf_output_extension}"
     trf_cmd = generic_trf_cmd.copy()
     trf_cmd[1] = region_fasta_file_path
     logger.info(" ".join(trf_cmd))
     subprocess.run(trf_cmd)
     create_trf_gtf(trf_output_file_path, region_results_file_path, region_name)
-    os.remove(trf_output_file_path)
-    os.remove(region_fasta_file_path)
+    trf_output_file_path.unlink()
+    region_fasta_file_path.unlink()
 
 
 def create_trf_gtf(trf_output_file_path, region_results_file_path, region_name):
+    """
+    Read the fasta file and save the content in gtf format
 
-    trf_in = open(trf_output_file_path, "r")
-    trf_out = open(region_results_file_path, "w+")
-    line = trf_in.readline()
-    repeat_count = 1
-    while line:
-        result_match = re.search(r"^\d+", line)
-        if result_match:
-            results = line.split()
-            if not len(results) == 15:
-                continue
-            start = results[0]
-            end = results[1]
-            period = float(results[2])
-            copy_number = float(results[3])
-            percent_matches = float(results[5])
-            score = float(results[7])
-            repeat_consensus = results[13]
-            if (
-                score < 50 and percent_matches >= 80 and copy_number > 2 and period < 10
-            ) or (copy_number >= 2 and percent_matches >= 70 and score >= 50):
-                gtf_line = (
-                    region_name
-                    + "\tTRF\trepeat\t"
-                    + str(start)
-                    + "\t"
-                    + str(end)
-                    + "\t.\t+\t.\t"
-                    + 'repeat_id "'
-                    + str(repeat_count)
-                    + '"; score "'
-                    + str(score)
-                    + '"; repeat_consensus "'
-                    + repeat_consensus
-                    + '";\n'
-                )
-                trf_out.write(gtf_line)
-                repeat_count += 1
-        line = trf_in.readline()
-    trf_in.close()
-    trf_out.close()
+    All the genomic slices are collected in a single gtf output
+    Args:
+        trf_output_file_path : pathlib.Path
+        region_results_file_path : pathlib.Path
+        region_name :str
+    """
+    with open(trf_output_file_path, "r") as trf_in, open(
+        region_results_file_path, "w+"
+    ) as trf_out:
+
+        repeat_count = 1
+        for line in trf_in:
+            result_match = re.search(r"^\d+", line)
+            if result_match:
+                results = line.split()
+                if not len(results) == 15:
+                    continue
+                start = results[0]
+                end = results[1]
+                period = float(results[2])
+                copy_number = float(results[3])
+                percent_matches = float(results[5])
+                score = float(results[7])
+                repeat_consensus = results[13]
+                if (
+                    score < 50
+                    and percent_matches >= 80
+                    and copy_number > 2
+                    and period < 10
+                ) or (copy_number >= 2 and percent_matches >= 70 and score >= 50):
+                    gtf_line = f'{region_name}\tTRF\trepeat\t{start}\t{end}\t.\t+\t.\trepeat_id "{repeat_count}"; score "{score}"; repeat_consensus "{repeat_consensus}";\n'
+
+                    trf_out.write(gtf_line)
+                    repeat_count += 1
 
 
 def run_red(red_path, main_output_dir, genome_file):
+    """
+    Run Red on genome file
 
+    Args:
+        red_path : str
+        main_output_dir : pathlib.Path
+        genome_file : pathlib.Path
+
+    Return:
+        masked genome file
+    """
     if not red_path:
         red_path = "Red"
 
     check_exe(red_path)
-    red_dir = create_dir(main_output_dir, "red_output")
-    red_mask_dir = create_dir(red_dir, "mask_output")
-    red_repeat_dir = create_dir(red_dir, "repeat_output")
-    red_genome_dir = create_dir(red_dir, "genome_dir")
+    red_dir = Path(create_dir(main_output_dir, "red_output"))
+    red_mask_dir = Path(create_dir(red_dir, "mask_output"))
+    red_repeat_dir = Path(create_dir(red_dir, "repeat_output"))
+    red_genome_dir = Path(create_dir(red_dir, "genome_dir"))
 
     sym_link_genome_cmd = "ln -s " + genome_file + " " + red_genome_dir
 
-    genome_file_name = os.path.basename(genome_file)
-    red_genome_file = os.path.join(red_genome_dir, genome_file_name)
-    masked_genome_file = os.path.join(
-        red_mask_dir, os.path.splitext(genome_file_name)[0] + ".msk"
-    )
-    repeat_coords_file = os.path.join(
-        red_repeat_dir, os.path.splitext(genome_file_name)[0] + ".rpt"
-    )
-    gtf_output_file_path = os.path.join(red_dir, "annotation.gtf")
+    genome_file_name = genome_file.name
+    red_genome_file = red_genome_dir / genome_file_name
+    genome_file_stem = genome_file.stem
+    masked_genome_file = red_mask_dir / f"{genome_file_stem}.msk"
+    repeat_coords_file = red_repeat_dir / f"{genome_file_stem}.rpt"
+    gtf_output_file_path = red_dir / "annotation.gtf"
 
-    if os.path.exists(masked_genome_file):
+    if masked_genome_file.exists():
         logger.warning(
             "Masked Genome file already found on the path to the Red mask output dir. Will not create a new file"
         )
         create_red_gtf(repeat_coords_file, gtf_output_file_path)
         return masked_genome_file
 
-    if os.path.exists(red_genome_file):
+    if red_genome_file.exists():
         logger.warning(
             "Unmasked genome file already found on the path to the Red genome dir, will not create a sym link"
         )
@@ -561,7 +554,7 @@ def run_red(red_path, main_output_dir, genome_file):
         )
         subprocess.run(["ln", "-s", genome_file, red_genome_dir])
 
-    if not os.path.exists(os.path.join(red_genome_dir, genome_file_name)):
+    if not red_genome_file.exists():
         logger.error(
             "Could not find the genome file in the Red genome dir or sym link to the original file. Path expected:\n%s"
             % red_genome_file
@@ -588,31 +581,22 @@ def run_red(red_path, main_output_dir, genome_file):
 
 
 def create_red_gtf(repeat_coords_file, gtf_output_file_path):
+    """
+    Create Red gtf file  from masked  genome file
 
-    red_in = open(repeat_coords_file, "r")
-    red_out = open(gtf_output_file_path, "w+")
-    line = red_in.readline()
-    repeat_id = 1
-    while line:
-        result_match = re.search(r"^\>(.+)\:(\d+)\-(\d+)", line)
-        if result_match:
-            region_name = result_match.group(1)
-            # Note that Red is 0-based, so add 1
-            start = int(result_match.group(2)) + 1
-            end = int(result_match.group(3)) + 1
-            gtf_line = (
-                region_name
-                + "\tRed\trepeat\t"
-                + str(start)
-                + "\t"
-                + str(end)
-                + "\t.\t+\t.\t"
-                + 'repeat_id "'
-                + str(repeat_id)
-                + '";\n'
-            )
-            red_out.write(gtf_line)
-            repeat_id += 1
-        line = red_in.readline()
-    red_in.close()
-    red_out.close()
+    Args:
+        repeat_coords_file: pathlib.Path
+        gtf_output_file_path : pathlib.Path
+    """
+    with open(repeat_coords_file, "r") as red_in, open(
+        gtf_output_file_path, "w+"
+    ) as red_out:
+        for repeat_id, line in enumerate(red_in, start=1):
+            result_match = re.search(r"^\>(.+)\:(\d+)\-(\d+)", line)
+            if result_match:
+                region_name = result_match.group(1)
+                # Note that Red is 0-based, so add 1
+                start = int(result_match.group(2)) + 1
+                end = int(result_match.group(3)) + 1
+                gtf_line = f'{region_name}\tRed\trepeat\t{start}\t{end}\t.\t+\t.\trepeat_id "{repeat_id}";\n'
+                red_out.write(gtf_line)
