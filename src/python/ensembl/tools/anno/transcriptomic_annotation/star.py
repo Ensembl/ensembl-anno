1148f325856756e81dcb84a0459fa6efe462ec98
‘ens-ftricomi’ <ftricomi@ebi.ac.uk>
2024-05-28 15:40:37 +0100

    sphinx fixed

--- src/python/ensembl/tools/anno/transcriptomic_annotation/star.py
+++ src/python/ensembl/tools/anno/transcriptomic_annotation/star.py
@@ -61,7 +61,6 @@ def run_star(  # pylint:disable=too-many-branches
 ) -> None:
     """
     Run STAR alignment on list of short read data.
-    
         :param genome_file: Genome file path.
         :type genome_file: Path
         :param output_dir: Working directory path.
