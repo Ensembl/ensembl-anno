c52393b662fb5d24e91269d5e666af037e1ea536
‘ens-ftricomi’ <ftricomi@ebi.ac.uk>
2026-07-13 23:36:35 +0100

    ignore oxford links

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,6 +67,9 @@ extensions = [
     "sphinxcontrib.bibtex",
 ]
 bibtex_bibfiles = ["references.bib"]
+linkcheck_ignore = [
+    r"https://doi\.org/.*",
+]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 highlight_language = "bash"
